d033d1781e887f87ddecc580c38fbca576d70dff
ivan <ivieee@yandex.ru>
2020-10-05 19:02:17 +0400

    Updates

---
+++ precache-manifest.b04a7b6ace9dfbaa98a4a7343eec47b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df2ed859ebd222489b77bad89725d3a",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "c361147c03a8ad1e0b97",
+    "url": "/react-weather-app/static/css/main.447645f9.chunk.css"
+  },
+  {
+    "revision": "6f0c6fae84a309f474f4",
+    "url": "/react-weather-app/static/js/2.0131e617.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.0131e617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c361147c03a8ad1e0b97",
+    "url": "/react-weather-app/static/js/main.49d6c4e8.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/react-weather-app/static/media/React-icon.05a3e30f.svg"
+  },
+  {
+    "revision": "c087e16aa3c587524d873d3f11e192e4",
+    "url": "/react-weather-app/static/media/search.c087e16a.svg"
+  }
+]);
